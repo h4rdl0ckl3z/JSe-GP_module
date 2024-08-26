42cb310b85f8048fd9e1319758d692dfb0f35855
Avatart0Dev <54482333+h4rdl0ckl3z@users.noreply.github.com>
2024-08-26 18:36:01 +0700

    Update e_GP.js

--- e_GP.js
+++ e_GP.js
@@ -1,13 +1,14 @@
 const http = require('http');
 const xml2js = require('xml2js');
 const iconv = require('iconv-lite');
+const pLimit = require('p-limit');
 
 async function e_GP(deptIds) {
     const baseUrl = 'http://process3.gprocurement.go.th/EPROCRssFeedWeb/egpannouncerss.xml';
     const announceTypes = ['W0', 'W2', 'B0', 'D0', 'D1', 'D2', 'P0', 'W1', '15'];
-
     const parser = new xml2js.Parser();
     const myData = [];
+    const limit = pLimit(10);  // Limit to 10 concurrent requests
 
     const fetchData = (url) => {
         return new Promise((resolve, reject) => {
@@ -33,26 +34,30 @@ async function e_GP(deptIds) {
         });
     };
 
+    const parseAndProcessData = async (response, announceType, deptId) => {
+        const parsedData = await parser.parseStringPromise(response);
+        const items = parsedData.rss.channel[0].item;
+
+        if (items) {
+            items.forEach(item => {
+                let listData = {};
+                for (const [key, value] of Object.entries(item)) {
+                    if (key !== 'description' && key !== 'guid') {
+                        listData[key] = value[0];
+                    }
+                }
+                listData['announceType'] = announceType;
+                listData['egpid'] = deptId;
+                myData.push(listData);
+            });
+        }
+    };
+
     const fetchAndParse = async (announceType, deptId) => {
         const url = `${baseUrl}?deptId=${deptId}&anounceType=${announceType}`;
         try {
             const response = await fetchData(url);
-            const parsedData = await parser.parseStringPromise(response);
-            const items = parsedData.rss.channel[0].item;
-
-            if (items) {
-                items.forEach(item => {
-                    let listData = {};
-                    for (const [key, value] of Object.entries(item)) {
-                        if (key !== 'description' && key !== 'guid') {
-                            listData[key] = value[0];
-                        }
-                    }
-                    listData['announceType'] = announceType;
-                    listData['egpid'] = deptId;
-                    myData.push(listData);
-                });
-            }
+            await parseAndProcessData(response, announceType, deptId);
         } catch (error) {
             console.error(`Error fetching or parsing XML for deptId: ${deptId}, announceType: ${announceType}`, error);
         }
@@ -62,8 +67,8 @@ async function e_GP(deptIds) {
         const tasks = [];
         for (const announceType of announceTypes) {
             for (const deptId of deptIds) {
-                if (deptId != '') {
-                    tasks.push(fetchAndParse(announceType, deptId));
+                if (deptId !== '') {
+                    tasks.push(limit(() => fetchAndParse(announceType, deptId)));
                 }
             }
         }
